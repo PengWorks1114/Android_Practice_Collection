6ac2d9cf8886799fa8fb2caeed0805dc120ab755
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-31 14:46:22 +0900

    創建物件

--- 07.CheckBox/app/src/main/java/com/example/checkbox/MainActivity.kt
+++ 07.CheckBox/app/src/main/java/com/example/checkbox/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.checkbox
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var checkBoxKotlin: CheckBox
+    private lateinit var checkBoxJava: CheckBox
+    private lateinit var checkBoxPython: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
