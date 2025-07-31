2690a7ffef880b67c30bd8eeef4c6e3cb89d5d38
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-31 14:26:47 +0900

    finish

--- 06.ToggleButton/app/src/main/java/com/example/togglebutton/MainActivity.kt
+++ 06.ToggleButton/app/src/main/java/com/example/togglebutton/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.togglebutton
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toggleButton: ToggleButton
+    private lateinit var textViewHide: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        toggleButton = findViewById(R.id.toggleButton)
+        textViewHide = findViewById(R.id.textView)
+
+        toggleButton.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if (isChecked) {
+                textViewHide.visibility = View.VISIBLE
+            } else {
+                textViewHide.visibility = View.INVISIBLE
+            }
+        }
+
     }
 }
