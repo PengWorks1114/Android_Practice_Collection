374fd4484c37947bebca8ad113976ef3c02490e1
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-01 15:03:05 +0900

    完成question layout xml

--- 10.QuizApp(Project_01)/app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
+++ 10.QuizApp(Project_01)/app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
@@ -1,11 +1,13 @@
 package com.example.quizapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.quizapp.R
+import com.example.quizapp.utils.Constants
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,8 @@ class QuestionsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val questionsList = Constants.getQuestions()
+        Log.d("QuestionSize", "${questionsList.size}")
     }
 }
