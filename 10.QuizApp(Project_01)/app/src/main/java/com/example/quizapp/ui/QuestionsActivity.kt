43ac5827c063aff4156223b14900d8398248416b
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-04 10:11:05 +0900

    新增註解

--- 10.QuizApp(Project_01)/app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
+++ 10.QuizApp(Project_01)/app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
@@ -2,6 +2,8 @@ package com.example.quizapp.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -13,7 +15,14 @@ import com.example.quizapp.R
 import com.example.quizapp.model.Question
 import com.example.quizapp.utils.Constants
 
-class QuestionsActivity : AppCompatActivity() {
+
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    //AppCompatActivity()	是 Android 中提供的 Activity 基底類別，支援舊版相容與 UI 功能
+//View.OnClickListener	是 Android 的「點擊事件監聽器介面」
+//讓這個類別能用 onClick() 方法處理元件的點擊事件
+//    這樣宣告之後，你就可以在這個 Activity 中直接寫：
+//    override fun onClick(view: View?) { ... }
+//    來統一處理 所有你註冊過的點擊事件，不需要為每個按鈕都寫一個 setOnClickListener { ... } 區塊。
     private lateinit var progressBar: ProgressBar
     private lateinit var textViewProgress: TextView
     private lateinit var textViewQuestion: TextView
@@ -23,8 +32,12 @@ class QuestionsActivity : AppCompatActivity() {
     private lateinit var textViewOptionTwo: TextView
     private lateinit var textViewOptionThree: TextView
     private lateinit var textViewOptionFour: TextView
+
+    private lateinit var checkButton: Button
+
     private val currentPosition = 1
     private lateinit var questionsList: MutableList<Question>
+    private val selectedOptionPosition: 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +53,7 @@ class QuestionsActivity : AppCompatActivity() {
         textViewProgress = findViewById(R.id.text_view_progress)
         textViewQuestion = findViewById(R.id.question_text_view)
         flagImage = findViewById(R.id.image_flag)
+        checkButton = findViewById(R.id.button_check)
 
         textViewOptionOne = findViewById(R.id.text_view_option_one)
         textViewOptionTwo = findViewById(R.id.text_view_option_two)
@@ -63,6 +77,19 @@ class QuestionsActivity : AppCompatActivity() {
         textViewOptionTwo.text = question.optionTwo
         textViewOptionThree.text = question.optionThree
         textViewOptionFour.text = question.optionFour
+
+        if(currentPosition == questionsList.size) {
+            checkButton.text = "FINISH"
+        } else {
+            checkButton.text = "CHECK"
+        }//當最後一題的時候改顯示成"FINISH"
+
+
+    }
+
+    override fun onClick(view: View?) {
+//        這一行是在實作 Android 中的點擊事件處理機制，屬於 Kotlin 的函式覆寫（override）語法 + Android 的 View 點擊處理架構。
+        when(view?.id)
     }
 
 }
